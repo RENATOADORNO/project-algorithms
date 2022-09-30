853b0c25ed768c0136d1ce3e2d21475a9f602023
Renato Adorno <renattoadorno@gmail.com>
2022-09-30 14:06:10 -0300

    feat: create find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if not nums or type(nums) is not list or len(nums) <= 1:
+        return False
+
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if type(nums[i]) is not int or nums[i] < 0:
+            return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
